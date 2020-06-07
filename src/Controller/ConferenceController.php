d679b0d194252aa7e660686a48b11a249bc1927e
Ivan Izotov <i.izotov@myfxchoice.com>
2020-06-07 19:58:33 +0300

    Добавление просмотра конференции и коментов

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,21 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ConferenceRepository;
+use Twig\Environment;
+use App\Repository\CommentRepository;
+use App\Entity\Conference;
 
 class ConferenceController extends AbstractController
 {
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(ConferenceRepository $conferenceRepository)
     {
-        return new Response(<<<EOF
-<html>
-  <body>
-    <img src="/images/under_construction.png" width="300px" />
-  </body>
-</html>
-EOF
-        );
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+    {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        ]));
     }
 }
