c5cf15c021947eb14d0545ca3d032bbf479a00cf
Ivan Izotov <i.izotov@myfxchoice.com>
2020-06-09 08:38:12 +0300

    add redis

--- docker/php/scripts/install_extensions.sh
+++ docker/php/scripts/install_extensions.sh
@@ -63,4 +63,5 @@ docker-php-ext-configure gd --with-freetype --with-jpeg \
      pcntl \
      mbstring \
   && docker-php-ext-configure zip && docker-php-ext-install zip \
-  && pecl install xdebug-2.8.1 && docker-php-ext-enable xdebug
+  && pecl install xdebug-2.8.1 && docker-php-ext-enable xdebug \
+  && pecl install -o -f redis &&  rm -rf /tmp/pear &&  docker-php-ext-enable redis
